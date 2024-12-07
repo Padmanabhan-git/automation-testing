7417344cc0e0efdfbd529fb938d633d88836335b
Padmanabhan-git <padmanabhans029@gmail.com>
2024-12-07 20:16:09 +0530

    Add files via upload

---
+++ Libs/CustomKeywords.groovy
@@ -0,0 +1,18 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import com.kms.katalon.core.testobject.TestObject
+
+import java.lang.String
+
+
+
+def static "com.qa.test.customfunction.CheckDropDownListElementExist"(
+    	TestObject object	
+     , 	String option	) {
+    (new com.qa.test.customfunction()).CheckDropDownListElementExist(
+        	object
+         , 	option)
+}
---
+++ Libs/internal/GlobalVariable.groovy
@@ -0,0 +1,36 @@
+package internal
+
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.main.TestCaseMain
+
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+public class GlobalVariable {
+     
+    /**
+     * <p></p>
+     */
+    public static Object url
+     
+    /**
+     * <p></p>
+     */
+    public static Object username
+     
+
+    static {
+        try {
+            def selectedVariables = TestCaseMain.getGlobalVariables("default")
+			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+            selectedVariables += TestCaseMain.getParsedValues(RunConfiguration.getOverridingParameters(), selectedVariables)
+    
+            url = selectedVariables['url']
+            username = selectedVariables['username']
+            
+        } catch (Exception e) {
+            TestCaseMain.logGlobalVariableError(e)
+        }
+    }
+}
